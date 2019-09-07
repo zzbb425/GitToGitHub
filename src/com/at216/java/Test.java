fc0e9d4eda6b1f554b014f2ffb6f4954a8e5ec8e
zzbb425 <1047441153@qq.com>
2019-09-07 22:57:36 +0800

    new test

--- src/com/at216/java/Test.java
+++ src/com/at216/java/Test.java
@@ -6,5 +6,9 @@ public class Test {
         for (int i = 0; i <10 ; i++) {
             System.out.println(i);
         }
+
+        for (int i = 0; i <100 ; i++) {
+            System.out.println(i+"aaa");
+        }
     }
 }
